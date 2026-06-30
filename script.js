64569965b3a908a366193af35dc27396b1d0f88e
italomazuco-dev <italo.mazuco@escola.pr.gov.br>
2026-06-29 21:34:46 -0300

    .

--- script.js
+++ script.js
@@ -1,15 +1,1 @@
-const botoes = document.querySelectorAll(".curtir");
-
-botoes.forEach(botao => {
-
-    let curtidas = 0;
-
-    botao.addEventListener("click", () => {
-
-        curtidas++;
-
-        botao.querySelector("span").textContent = curtidas;
-
-    });
-
-});
+alert("Bem-vindo ao Projeto Agro!");
